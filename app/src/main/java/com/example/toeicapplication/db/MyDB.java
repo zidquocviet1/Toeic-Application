d91cc3d5a6f91731ba89cda048c1dfb1fb98aef2
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-06-14 15:41:30 +0700

    add WorkerManager

--- app/src/main/java/com/example/toeicapplication/db/MyDB.java
+++ app/src/main/java/com/example/toeicapplication/db/MyDB.java
@@ -7,6 +7,9 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 import com.example.toeicapplication.db.dao.CourseDAO;
 import com.example.toeicapplication.db.dao.QuestionDAO;
@@ -16,18 +19,9 @@ import com.example.toeicapplication.model.Course;
 import com.example.toeicapplication.model.Question;
 import com.example.toeicapplication.model.User;
 import com.example.toeicapplication.model.Word;
-import com.example.toeicapplication.utilities.Utils;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.example.toeicapplication.workers.SeedDatabaseWorker;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 @Database(entities = {User.class,
         Course.class,
@@ -50,60 +44,11 @@ public abstract class MyDB extends RoomDatabase {
                     @Override
                     public void onCreate(@NonNull @NotNull SupportSQLiteDatabase db) {
                         super.onCreate(db);
-                        Executors
-                                .newSingleThreadScheduledExecutor()
-                                .execute(() -> {
-                                    getInstance(context).getCourseDAO().insertAll(Course.courses);
-                                    List<Word> words = getWordFromAssets(context);
-                                    List<Question> questions = getQuestionFromAssets(context);
-
-                                    getInstance(context).getWordDAO().insertAll(words);
-                                    getInstance(context).getQuestionDAO().insertAll(questions);
-                                });
+                        WorkRequest request = OneTimeWorkRequest.from(SeedDatabaseWorker.class);
+                        WorkManager.getInstance(context).enqueue(request);
                     }
                 })
                 .build();
-    }
-
-    private static List<Word> getWordFromAssets(Context context) {
-        try {
-            InputStream is = context.getAssets().open("vocab_json.json");
-            String jsonFileString = Utils.getJsonFromAssets(is);
-
-            Gson gson = new Gson();
-            Type listWordType = new TypeToken<List<Word>>() {
-            }.getType();
-
-            return gson.fromJson(jsonFileString, listWordType);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private static List<Question> getQuestionFromAssets(Context context) {
-        try {
-            InputStream is = context.getAssets().open("question.json");
-            String jsonFileString = Utils.getJsonFromAssets(is);
-
-            Gson gson = new Gson();
-            Type listQuestionType = new TypeToken<List<Question>>() {
-            }.getType();
-            List<Question> questions = gson.fromJson(jsonFileString, listQuestionType);
-
-            if (questions != null && !questions.isEmpty()) {
-                return questions.stream()
-                        .map(q -> {
-                            q.setAudioFile("course_" + q.getCourseID() + "/" + q.getAudioFile());
-                            return q;
-                        })
-                        .collect(Collectors.toList());
-            }
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
     public MyDB() {
--- app/src/main/java/com/example/toeicapplication/workers/SeedDatabaseWorker.java
+++ app/src/main/java/com/example/toeicapplication/workers/SeedDatabaseWorker.java
@@ -1,4 +1,93 @@
 package com.example.toeicapplication.workers;
 
-public class SeedDatabaseWorker{
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.toeicapplication.db.MyDB;
+import com.example.toeicapplication.model.Course;
+import com.example.toeicapplication.model.Question;
+import com.example.toeicapplication.model.Word;
+import com.example.toeicapplication.utilities.Utils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SeedDatabaseWorker extends Worker {
+    public SeedDatabaseWorker(@NonNull @NotNull Context context,
+                              @NonNull @NotNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Result doWork() {
+        try {
+            Context context = getApplicationContext();
+
+            MyDB db = MyDB.getInstance(context);
+
+            List<Word> words = getWordFromAssets(context);
+            List<Question> questions = getQuestionFromAssets(context);
+
+            db.getCourseDAO().insertAll(Course.courses);
+            db.getWordDAO().insertAll(words);
+            db.getQuestionDAO().insertAll(questions);
+
+            return Result.success();
+        }catch (Exception e) {
+            return Result.failure();
+        }
+    }
+
+    private static List<Word> getWordFromAssets(Context context) {
+        try {
+            InputStream is = context.getAssets().open("vocab_json.json");
+            String jsonFileString = Utils.getJsonFromAssets(is);
+
+            Gson gson = new Gson();
+            Type listWordType = new TypeToken<List<Word>>() {
+            }.getType();
+
+            return gson.fromJson(jsonFileString, listWordType);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static List<Question> getQuestionFromAssets(Context context) {
+        try {
+            InputStream is = context.getAssets().open("question.json");
+            String jsonFileString = Utils.getJsonFromAssets(is);
+
+            Gson gson = new Gson();
+            Type listQuestionType = new TypeToken<List<Question>>() {
+            }.getType();
+            List<Question> questions = gson.fromJson(jsonFileString, listQuestionType);
+
+            if (questions != null && !questions.isEmpty()) {
+                return questions.stream()
+                        .map(q -> {
+                            q.setAudioFile("course_" + q.getCourseID() + "/" + q.getAudioFile());
+                            return q;
+                        })
+                        .collect(Collectors.toList());
+            }
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
