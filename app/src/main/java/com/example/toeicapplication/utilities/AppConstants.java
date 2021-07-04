3fe1f191a7c74671bba531304b04eb55430220c7
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-07-04 16:03:04 +0700

    init

---
+++ app/src/main/java/com/example/toeicapplication/utilities/AppConstants.java
@@ -0,0 +1,9 @@
+package com.example.toeicapplication.utilities;
+
+public class AppConstants {
+    // network
+    public static final String API_ENDPOINT = "http://192.168.100.15:8080/api/v1/";
+    public static final String CONTENT_TYPE = "application/json";
+
+    public static final int NETWORK_TIME_OUT = 10;
+}
