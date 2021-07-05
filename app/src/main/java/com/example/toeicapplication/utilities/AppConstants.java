69e8bfe09b4f7a0ae0fd2ec4103c65ee238e0689
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-07-05 13:57:24 +0700

    add new Constant

--- app/src/main/java/com/example/toeicapplication/utilities/AppConstants.java
+++ app/src/main/java/com/example/toeicapplication/utilities/AppConstants.java
@@ -1,9 +1,11 @@
 package com.example.toeicapplication.utilities;
 
 public class AppConstants {
-    // network
+    // NETWORK
     public static final String API_ENDPOINT = "http://192.168.100.15:8080/api/v1/";
     public static final String CONTENT_TYPE = "application/json";
-
     public static final int NETWORK_TIME_OUT = 10;
+
+    // LOGGING
+    public static final String TAG = "Toeic Application";
 }
