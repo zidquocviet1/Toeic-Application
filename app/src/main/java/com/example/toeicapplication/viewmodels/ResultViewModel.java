3353ed437f64f4048445e727fe4f6c4b964d2d45
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-07-07 22:50:15 +0700

    addResult function

--- app/src/main/java/com/example/toeicapplication/viewmodels/ResultViewModel.java
+++ app/src/main/java/com/example/toeicapplication/viewmodels/ResultViewModel.java
@@ -6,14 +6,27 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.toeicapplication.model.entity.Result;
+import com.example.toeicapplication.network.response.MyResponse;
 import com.example.toeicapplication.repository.ResultRepository;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.net.HttpURLConnection;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
+import retrofit2.Response;
 
 @HiltViewModel
 public class ResultViewModel extends ViewModel {
@@ -35,19 +48,44 @@ public class ResultViewModel extends ViewModel {
     }
 
     public void add(Result result) {
-        cd.add(repository
+        Disposable addResultToDatabase = repository
                 .add(result)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(() -> {
-                    Log.e("TAG", "Insert result successfully!");
-                })
-        );
+                .subscribe(() -> Log.e("TAG", "Insert result successfully!"));
+
+        Disposable addResultRemote = repository
+                .addResultRemote(result)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribeWith(new DisposableObserver<Response<MyResponse<Result>>>() {
+                    @Override
+                    public void onNext(@NotNull Response<MyResponse<Result>> response) {
+                        MyResponse<Result> body = response.body();
+
+                        if (body != null) {
+                            Log.e("TAG", body.getMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NotNull Throwable e) {
+                        Log.e("TAG", e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+        cd.add(addResultToDatabase);
+        cd.add(addResultRemote);
     }
 
     @Override
     protected void onCleared() {
         super.onCleared();
-        cd.clear(); // this is not accept new observable
+//        cd.clear(); // this is not accept new observable
     }
 }
