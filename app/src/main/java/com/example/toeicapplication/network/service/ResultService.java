2129a9dd625be013ec7f74d7d71167d25d07bd11
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-07-07 22:52:37 +0700

    add remote function to service classes

--- app/src/main/java/com/example/toeicapplication/network/service/ResultService.java
+++ app/src/main/java/com/example/toeicapplication/network/service/ResultService.java
@@ -1,13 +1,14 @@
 package com.example.toeicapplication.network.service;
 
 import com.example.toeicapplication.model.entity.Result;
-import com.example.toeicapplication.network.response.Response;
+import com.example.toeicapplication.network.response.MyResponse;
 
 import io.reactivex.Observable;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ResultService {
-    @POST(value = "result/add")
-    Observable<Response<Result>> add(@Body Result result);
+    @POST(value = "result")
+    Observable<Response<MyResponse<Result>>> add(@Body Result result);
 }
--- app/src/main/java/com/example/toeicapplication/network/service/UserService.java
+++ app/src/main/java/com/example/toeicapplication/network/service/UserService.java
@@ -1,25 +1,33 @@
 package com.example.toeicapplication.network.service;
 
 import com.example.toeicapplication.model.entity.User;
-import com.example.toeicapplication.network.response.Response;
+import com.example.toeicapplication.network.response.MyResponse;
+
+import java.util.List;
 
 import io.reactivex.Observable;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface UserService {
     @POST(value = "user/login")
-    Observable<Response<User>> login(@Body User user);
+    Observable<MyResponse<User>> login(@Body User user);
 
     @PUT(value  = "user")
-    Observable<Response<User>> logout(@Body User user);
+    Observable<MyResponse<User>> logout(@Body User user);
 
     @POST(value = "user/register")
-    Observable<Response<User>> register(@Body User user);
+    Observable<MyResponse<User>> register(@Body User user);
 
     @GET(value = "user/{id}")
-    Observable<Response<User>> findUser(@Path("id") Long id);
+    Observable<MyResponse<User>> findUser(@Path("id") Long id);
+
+    @GET(value = "user/leaderboard")
+    Observable<Response<MyResponse<List<User>>>> leaderboard(@Header("USER_API_KEY") String key, @Query("courseId") Long courseId);
 }
