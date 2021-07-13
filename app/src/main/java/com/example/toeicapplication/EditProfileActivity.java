e3b84d647ee7bb61b10ad5afb387f375bd376ac4
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-07-13 22:04:20 +0700

    update User Profile and User Edit Profile

--- app/src/main/java/com/example/toeicapplication/EditProfileActivity.java
+++ app/src/main/java/com/example/toeicapplication/EditProfileActivity.java
@@ -1,14 +1,56 @@
 package com.example.toeicapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.toeicapplication.databinding.ActivityEditProfileBinding;
+import com.example.toeicapplication.model.entity.User;
+import com.example.toeicapplication.utilities.DatePickerValidator;
+import com.example.toeicapplication.utilities.ExifUtils;
+import com.example.toeicapplication.utilities.MyActivityForResult;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.io.FileNotFoundException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class EditProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+
+    private final MyActivityForResult<String, Boolean> permissionLauncher =
+            MyActivityForResult.registerActivityForResult(this, new ActivityResultContracts.RequestPermission());
+
+    private final MyActivityForResult<Intent, ActivityResult> resultLauncher =
+            MyActivityForResult.registerActivityForResult(this);
+
     private ActivityEditProfileBinding binding;
+    private LocalDateTime birthday = null;
+
+    interface ResultCallback{
+        void onActivityResult(ActivityResult result);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,29 +58,136 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
         binding = ActivityEditProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Intent intent = getIntent();
+        User user = intent.getParcelableExtra("user");
+
+        if (user != null) showInfo(user);
+
         registerOnClickEvent();
     }
 
-    private void registerOnClickEvent(){
+    private void showInfo(User user) {
+        binding.edtName.setText(user.getDisplayName());
+    }
+
+    private void registerOnClickEvent() {
         binding.imgAvatar.setOnClickListener(this);
         binding.layout1.setOnClickListener(this);
         binding.txtCancel.setOnClickListener(this);
         binding.txtSave.setOnClickListener(this);
+        binding.edtBirthday.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
 
-        if (id == binding.imgAvatar.getId()){
-            // check the camera and the internal file permission
-            // if the permission is granted then open the dialog to choose image
-        }else if (id == binding.layout1.getId()){
+        if (id == binding.imgAvatar.getId() || id == binding.layout1.getId()) {
+            showPopup();
+        } else if (id == binding.layout1.getId()) {
             // check the permission like Avatar but this is change the panel background
-        }else if (id == binding.txtCancel.getId()){
+            showPopup();
+        } else if (id == binding.txtCancel.getId()) {
             this.finish();
-        }else if (id == binding.txtSave.getId()){
+        } else if (id == binding.txtSave.getId()) {
             // save new user information to the database
+        } else if (id == binding.edtBirthday.getId()) {
+            showDatePicker();
         }
+    }
+
+    private void showPopup() {
+        MaterialAlertDialogBuilder alertDialog = new MaterialAlertDialogBuilder(this);
+
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            alertDialog.setPositiveButton(R.string.take_photo, (dialog, which) -> takePhoto());
+        }
+        alertDialog.setNegativeButton(R.string.choose_existing_photo, (dialog, which) -> chooseExistingPhoto());
+        alertDialog.create().show();
+    }
+
+    private void takePhoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        requestPermission(Manifest.permission.CAMERA, takePictureIntent, result -> {
+            Intent data = result.getData();
+            if (data != null) {
+                Bundle bundle = data.getExtras();
+                if (bundle != null) {
+                    Bitmap imageBitmap = (Bitmap) bundle.get("data");
+                    binding.imgAvatar.setImageBitmap(imageBitmap);
+                }
+            }
+        });
+    }
+
+    private void chooseExistingPhoto() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            if (Build.VERSION.SDK_INT < 29){
+                requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE, intent, this::getImageBitmapFromResult);
+            }else {
+                resultLauncher.mLaunch(intent, this::getImageBitmapFromResult);
+            }
+        }
+    }
+
+    private void getImageBitmapFromResult(ActivityResult result){
+        Intent data = result.getData();
+        if (data != null){
+            Uri fullPhotoUri = data.getData();
+            Bitmap rotatedBitmap = ExifUtils.getRotatedBitmap(getContentResolver(), fullPhotoUri);
+            binding.imgAvatar.setImageBitmap(rotatedBitmap);
+        }
+    }
+
+    private void showDatePicker() {
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+
+        calendar.setTimeInMillis(MaterialDatePicker.todayInUtcMilliseconds());
+        long today = calendar.getTimeInMillis();
+
+        CalendarConstraints constraints = new CalendarConstraints.Builder()
+                .setEnd(today)
+                .setValidator(DatePickerValidator.from(today))
+                .build();
+
+        MaterialDatePicker<Long> picker = MaterialDatePicker.Builder.datePicker()
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setTitleText("Select the birthday")
+                .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+                .setCalendarConstraints(constraints)
+                .build();
+
+        picker.setCancelable(false);
+        picker.addOnPositiveButtonClickListener(selection -> {
+            LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(selection), ZoneOffset.UTC);
+            String date = dateTime.format(formatter);
+
+            birthday = dateTime;
+            binding.edtBirthday.setText(date);
+        });
+        picker.show(getSupportFragmentManager(), "picker");
+    }
+
+    private void requestPermission(String permissionString, Intent intent, ResultCallback callback) {
+        if (ContextCompat.checkSelfPermission(this, permissionString) == PackageManager.PERMISSION_GRANTED) {
+            startResultLauncher(intent, callback);
+        } else {
+            // use shouldShowRequestPermissionRationale can customize the dialog
+            permissionLauncher.mLaunch(permissionString, isGranted -> {
+                if (isGranted) {
+                    startResultLauncher(intent, callback);
+                }
+            });
+        }
+    }
+
+    private void startResultLauncher(Intent intent, ResultCallback callback) {
+        resultLauncher.mLaunch(intent, result -> {
+            if (callback != null) callback.onActivityResult(result);
+        });
     }
 }
--- app/src/main/java/com/example/toeicapplication/UserActivity.java
+++ app/src/main/java/com/example/toeicapplication/UserActivity.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 
+import androidx.activity.result.ActivityResult;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.toeicapplication.databinding.ActivityUserBinding;
 import com.example.toeicapplication.model.entity.Result;
 import com.example.toeicapplication.model.entity.User;
+import com.example.toeicapplication.utilities.AppConstants;
+import com.example.toeicapplication.utilities.MyActivityForResult;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class UserActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityUserBinding binding;
+    private User user;
+    private final MyActivityForResult<Intent, ActivityResult> activityLauncher = MyActivityForResult.registerActivityForResult(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +28,21 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityUserBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        receiveUserAndShowInfo();
+        Intent intent = getIntent();
+        user = intent.getParcelableExtra("user");
+
+        showInfo(user);
         binding.imgAvatar.setOnClickListener(this);
         binding.btnEdit.setOnClickListener(this);
     }
 
-    private void receiveUserAndShowInfo(){
-        Intent intent = getIntent();
-
-        User user = intent.getParcelableExtra("user");
-
+    private void showInfo(User user){
         if (user != null){
             List<Result> results = user.getResults();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(AppConstants.DATE_TIME_PATTERN);
 
             binding.txtDisplayName.setText(user.getDisplayName());
-            binding.txtJoin.setText(getString(R.string.join, user.getTimestamp().format(DateTimeFormatter.ISO_LOCAL_DATE)));
+            binding.txtJoin.setText(getString(R.string.join, user.getTimestamp().format(formatter)));
 
             if (results != null){
                 binding.txtRecord.setText(Html.fromHtml(getString(R.string.record, results.size()), Html.FROM_HTML_MODE_COMPACT));
@@ -58,7 +63,17 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         int id = v.getId();
 
         if (id == binding.btnEdit.getId()){
-            startActivity(new Intent(UserActivity.this, EditProfileActivity.class));
+            Intent intent = new Intent(UserActivity.this, EditProfileActivity.class);
+            intent.putExtra("user", user);
+
+            activityLauncher.mLaunch(intent, result -> {
+                Intent response = result.getData();
+
+                if (response != null){
+                    User user = response.getParcelableExtra("user");
+                    if (user != null) showInfo(user);
+                }
+            });
         }else if (id == binding.imgAvatar.getId()){
 
         }
