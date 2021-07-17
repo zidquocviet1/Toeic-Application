7ea288f151a8f47cf8c68cd08bc2bad8d3efd759
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-07-17 20:39:56 +0700

    add PUT request multipart

--- app/src/main/java/com/example/toeicapplication/network/service/UserService.java
+++ app/src/main/java/com/example/toeicapplication/network/service/UserService.java
@@ -6,12 +6,17 @@ import com.example.toeicapplication.network.response.MyResponse;
 import java.util.List;
 
 import io.reactivex.Observable;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -31,4 +36,16 @@ public interface UserService {
     @GET(value = "user/leaderboard")
     Observable<Response<MyResponse<List<User>>>> leaderboard(@Header("USER_API_KEY") String key,
                                                                      @Query("courseId") Long courseId);
+
+    @Multipart
+    @PUT(value = "user/edit_profile")
+    Observable<Response<MyResponse<User>>> editProfile(@Part MultipartBody.Part avatar,
+                                                 @Part("userId") RequestBody userId,
+                                                 @Part("fullName") RequestBody fullName,
+                                                 @Part("bio") RequestBody bio,
+                                                 @Part("birthday") RequestBody birthday,
+                                                 @Part("address") RequestBody address);
+
+    @PUT(value = "user/edit_profile_without_image")
+    Observable<Response<MyResponse<User>>> editProfile(@Body User user);
 }
